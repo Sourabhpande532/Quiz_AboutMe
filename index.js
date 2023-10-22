23cb184e3de78783779590eb0d91228694c8cc52
sourabh <sourabhpande43@gmail.comm>
2023-10-22 16:30:24 +0530

    refactor - everything related code

--- index.js
+++ index.js
@@ -1,74 +1,62 @@
-/* 
-@Challenge:
--use question list
--and function to put together a quiz*/
+const readlinesync = require("readline-sync");
 
-const readlineSync = require('readline-sync');
-const userQuestion = readlineSync.question("Where do you live ?");
-console.log(" Welcome to our Zone " + userQuestion)
 let score = 0;
-function play(question,answer){
-const userQuestion = readlineSync.question(question);
-  if(userQuestion === answer){
-    console.log("You are Right !")
-    score = score + 1;
-    console.log("Current score is : ", score)
-  }else{
-    console.log("You are Wrong !")
+
+function playQuiz(question, answer) {
+  const userQuestion = readlinesync.question(question)
+  if (userQuestion === answer) {
+    console.log("You are right")
+    score = score + 1
+    console.log("-------------")
+    console.log("Your current score is ", score)
+  } else {
+    console.log("You are wrong")
+    console.log("-------------")
   }
 }
 
-const questions = [
+const sequenceViseQues = [
   {
-  question: "is it city or town ?",
-  answer: "town"
-},{
-  question:" which zone do you live ?",
-  answer: "gopal sq"
-},{
-  question:" is it near tea house ?",
-  answer: "yes"
-}]
+    question: `What's your Name May I know ? 
+  a:ram,
+  b:sham,
+  c:jems`,
+    answer: "jems"
+  },
+  {
+    question: `How old you are ? 
+  a:55,
+  b:67,
+  c:66`,
+    answer: "55"
+  },
+  {
+  question: `what's your favouorite meal ? 
+  a:haldiram,
+  b:chips,
+  c:nuts `,
+    answer: "nuts"
+  },
+  {
+  question: `what's your Hobby ? 
+  a:Cricket,
+  b:Meditation,
+  c:Music `,
+    answer: "cricket"
+  },
+  {
+  question: `are you married ? 
+  a:Yes,
+  b:No `,
+    answer: "No"
+  }
+]
 
-for (let i=0; i<questions.length; i++){
- const currOne = questions[i];
- play(currOne.question,currOne.answer) 
+for (let i = 0; i < sequenceViseQues.length; i++) {
+  let curr = sequenceViseQues[i];
+  // console.log(curr.question, curr.answer)  
+  playQuiz(curr.question, curr.answer)
+
 }
 
-console.log(" YEH..YOU Scored Well : ", score)
-
-
-
-
-
-/* let readLine = require("readline-sync");
-let username = readLine.question("Hello,What is your name? ");
-console.log("Hey "+ username +" Let's see how much you know sourabh")
-let score = 0;
-let totalScore = 0;
-function ask(question,answer){
-  let userAnswer = readLine.question(question);
-  if(userAnswer.toLowerCase() === answer.toLowerCase()){
-    console.log("You are right!");
-    score++;
-    
-  }
-  else{
-    console.log("You are wrong!!");
-    
-    
-  }
-  console.log("Your score is "+ score );
-  console.log("----------");
-  totalScore++;
-}
-
-let questionOne="Where do Sourabh live? ";
-let questionTwo = "Which lifestyle suitable for sourabh? ";
-let questionThree = "Which outdoor game does Sourabh like to play? ";
-
-ask(questionOne,"Bhiwapur");
-ask(questionTwo,"Satvik");
-ask(questionThree,"Cricket");
-
-console.log("Hooray!!Your final score is "+ score + " out of "+ totalScore); */
+console.log("Hey, You total score is ", score)
