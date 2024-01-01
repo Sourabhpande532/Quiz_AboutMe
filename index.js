a025c190735f2552ff637814786bd6774ddb2f6d
Saurabh pande <93812514+Sourabhpande532@users.noreply.github.com>
2024-01-01 11:19:48 +0530

    refactor - bring some changes

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const readlinesync = require("readline-sync");
 let score = 0;
 
 function playQuiz(question, answer) {
-  const userQuestion = readlinesync.question(question)
+  const userQuestion = readlinesync.question(question);
   if (userQuestion === answer) {
-    console.log("You are right")
-    score = score + 1
-    console.log("-------------")
-    console.log("Your current score is ", score)
+    console.log("You are right");
+    score = score + 1;
+    console.log("-------------");
+    console.log("Your current score is ", score);
   } else {
-    console.log("You are wrong")
-    console.log("-------------")
+    console.log("You are wrong");
+    console.log("-------------");
   }
 }
 
@@ -21,42 +21,40 @@ const sequenceViseQues = [
   a:ram,
   b:sham,
   c:jems`,
-    answer: "jems"
+    answer: "jems",
   },
   {
     question: `How old you are ? 
   a:55,
   b:67,
   c:66`,
-    answer: "55"
+    answer: "55",
   },
   {
-  question: `what's your favouorite meal ? 
+    question: `what's your favouorite meal ? 
   a:haldiram,
   b:chips,
   c:nuts `,
-    answer: "nuts"
+    answer: "nuts",
   },
   {
-  question: `what's your Hobby ? 
+    question: `what's your Hobby ? 
   a:Cricket,
   b:Meditation,
   c:Music `,
-    answer: "cricket"
+    answer: "cricket",
   },
   {
-  question: `are you married ? 
+    question: `are you married ? 
   a:Yes,
   b:No `,
-    answer: "No"
-  }
-]
+    answer: "No",
+  },
+];
 
 for (let i = 0; i < sequenceViseQues.length; i++) {
   let curr = sequenceViseQues[i];
-  // console.log(curr.question, curr.answer)  
-  playQuiz(curr.question, curr.answer)
-
+  playQuiz(curr.question, curr.answer);
 }
 
-console.log("Hey, You total score is ", score)
+console.log("Hey, You total score is ", score);
